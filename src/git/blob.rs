cfecc67e9e759b8c1d3f8ddf5e1d043b4ec8b43c
sayandas-sd <sayandasmac@gmail.com>
2025-08-18 23:53:12 +0530

    [feat]: added blob that handle content with proper SHA algo

---
+++ src/git/blob.rs
@@ -0,0 +1,23 @@
+
+use::std::{ io, num, string };
+
+
+pub struct Blob {
+    pub content: Vec<u8>,
+    pub content_size: i32,
+
+}
+
+
+impl Blob {
+    fn create_new_blob(content: Vec<u8>) -> Self {
+
+    }
+    fn get_content() -> String {
+
+    }
+
+    fn get_blob() -> Blob {
+
+    }
+}
