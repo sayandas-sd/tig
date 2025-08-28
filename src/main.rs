ec850b972270b08543cd345b9d7f4fe57c4e2208
sayandas-sd <sayandasmac@gmail.com>
2025-08-28 23:54:39 +0530

    feat(cli): add clap based commands that manage files with subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
-#[command(propagate_version = true)]
+#[command(version, about, long_about = None, propagate_version = true)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -12,13 +11,33 @@ struct Cli {
 enum Commands {
    Init,
    Create{ branch: String },
-   Commit{ message: String },
+   Commit{ 
+        #[arg(short, long, default_value = "")]
+        message: String 
+    },
    Cat{ hash: String },
    Delete{ branch: String },
    Log,
-   Revert{ branch_location: String, branch_level: String }
+   Revert{ 
+        #[arg(short, long, default_value = "1")]
+        branch_location: u32, 
+        branch_level: String 
+   }
 }
 
 fn main() {
      let cli = Cli::parse();
+
+    
+    match &cli.command {
+            Commands::Init => println!("init repo"),
+            Commands::Create { branch } => println!("create branch {}", branch),
+            Commands::Commit { message } => println!("commit message {}", message),
+            Commands::Cat { hash } => println!("objects {}", hash),
+            Commands::Delete { branch } => println!("delete branch {}", branch),
+            Commands::Log => println!("Show log"),
+            Commands::Revert { branch_location, branch_level } => {
+                println!("Revert branch {} at level {}", branch_level, branch_location);
+            }
+        }
 }
